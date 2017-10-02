d62e88b75a3df899cdeb4c83ce72e49e75f1ded9
U-MEGATRENDS.COM\joshuac <joshuac@JoshuaC-DELL-PC.us.megatrends.com>
2017-10-02 18:08:53 -0400

    Made some updates to how the inputs are handeled, also started adding code for plotting prices

--- BittrexGUI.py
+++ BittrexGUI.py
@@ -1,10 +1,18 @@
 #!/usr/bin/env python
-from Bittrex_price_caller import bittrex
-
 try:
     from Tkinter import *
 except ImportError:
     from tkinter import *
+import matplotlib.animation as animation
+import matplotlib
+# from matplotlib import style as style
+matplotlib.use("TkAgg")
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2TkAgg
+from matplotlib.figure import Figure
+
+from Bittrex_price_caller import bittrex
+
+# style.use("ggplot")
 
 
 class BittrexGUI:
@@ -12,7 +20,8 @@ class BittrexGUI:
         self.master = master
         master.title("Bittrex Price Checker")
 
-        self.label = Label(master, text="Enter the ticker for any coin and click Show Price")
+        self.label = Label(
+            master, text="Enter the ticker for any coin and click Show Price")
         self.label.pack()
 
         self.pricelabel = Label(self.master)
@@ -32,12 +41,12 @@ class BittrexGUI:
         api = bittrex()
         trade = 'BTC'
         if self.e.get() == "":
-            coincurrency = 'OMG'
+            coincurrency = 'ETH'
         else:
             coincurrency = self.e.get()
         market = '{0}-{1}'.format(trade, coincurrency)
-        omgsummary = api.getmarketsummary(market)
-        omgprice = omgsummary[0]['Last']
+        coinsummary = api.getmarketsummary(market)
+        coinprice = coinsummary[0]['Last']
         # print 'The price for {0} is {1:.8f} {2}.'.format(currency, omgprice, trade)
 
         trade = 'USDT'
@@ -46,12 +55,39 @@ class BittrexGUI:
         btcsummary = api.getmarketsummary(market)
         btcprice = btcsummary[0]['Last']
         # print 'The price for {0} is {1:.8f} {2}.'.format(coincurrency, btcprice, trade)
-        dollaPrice = omgprice * btcprice
+        dollaPrice = coinprice * btcprice
         # print 'The $ price for OMG is {0:2.3f}'.format(dollaPrice)
         self.pricelabel.config(
             text="The $ price for {0} is {1:2.3f}".format(coincurrency, dollaPrice))
         # self.pricelabel.pack()
-        self.master.after(300000, self.update_price)  # 300000, update_price)
+
+        self.f = Figure(figsize=(5, 5), dpi=100)
+        self.a = self.f.add_subplot(111)
+        # a.plot([1, 2, 3, 4, 5, 6, 7, 8], [5, 6, 1, 3, 8, 9, 3, 5])
+
+        self.canvas = FigureCanvasTkAgg(self.f, self.master)
+        self.canvas.show()
+        self.canvas.get_tk_widget().pack(expand=True)
+        self.canvas._tkcanvas.pack()
+
+        self.ani = animation.FuncAnimation(
+            my_gui.f, my_gui.animate, interval=100)
+        # self.master.after(300000, self.update_price)  # 300000, update_price)
+
+    def animate(self, i):
+        api = bittrex()
+        trade = 'BTC'
+        if self.e.get() == "":
+            coincurrency = 'ETH'
+        else:
+            coincurrency = self.e.get()
+        market = '{0}-{1}'.format(trade, coincurrency)
+        coinsummary = api.getmarketsummary(market)
+        coinprice = coinsummary[0]['Last']
+
+        self.a.clear()
+
+        self.a.plot(coinprice)
 
 
 root = Tk()
--- CoinPrice.py
+++ CoinPrice.py
@@ -4,17 +4,32 @@ from Bittrex_price_caller import bittrex
 
 api = bittrex()
 trade = 'BTC'
-currency = sys.argv[1]
-market = '{0}-{1}'.format(trade, currency)
-omgsummary = api.getmarketsummary(market)
-omgprice = omgsummary[0]['Last']
-print ('The price for {0} is {1:.8f} {2}.'.format(currency, omgprice, trade))
+if len(sys.argv) == 1:
+    coincurrency = 'ETH'
+else:
+    coincurrency = sys.argv[1]
+market = '{0}-{1}'.format(trade, coincurrency)
+coinsummary = api.getmarketsummary(market)
+coinprice = coinsummary[0]['Last']
+print('The price for {0} is {1:.8f} {2}.'.format(
+    coincurrency, coinprice, trade))
 
 trade = 'USDT'
 currency = 'BTC'
 market = '{0}-{1}'.format(trade, currency)
 btcsummary = api.getmarketsummary(market)
 btcprice = btcsummary[0]['Last']
-print ("The price for {0} is {1:.8f} {2}.".format(currency, btcprice, trade))
-dollaPrice = omgprice * btcprice
-print ('The $ price for OMG is {0:2.3f}'.format(dollaPrice))
+# print(btcsummary)
+# print(btcsummary[0]['Last'])
+# print ("The price for {0} is {1:.2f} {2}.".format(currency, btcprice, trade))
+dollaPrice = coinprice * btcprice
+print ('The $ price for {0} is {1:2.3f}'.format(coincurrency, dollaPrice))
+
+trade = 'BTC'
+market = '{0}-{1}'.format(trade, coincurrency)
+btcsummary = api.getmarkethistory(market)
+x = 0
+for x in(range(5)):
+    coinprice = btcsummary[x]['Price']
+    dollaPrice = coinprice * btcprice
+    print('{0:.4f}'.format(dollaPrice))
